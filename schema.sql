d7515e4d383e4999d2401859c6fc03f7bb439901
Amaka Konwea <lawrahcollections@gmail.com>
2023-03-10 09:01:49 +0000

    Query multiple tables

--- schema.sql
+++ schema.sql
@@ -11,3 +11,33 @@ CREATE TABLE animals (
 
 ALTER TABLE animals
 ADD COLUMN species VARCHAR(150);
+
+-- Create multiple tables
+
+CREATE TABLE owners (
+    id       SERIAL PRIMARY KEY,
+    fullname VARCHAR(200) NOT NULL,
+    age      INT NOT NULL
+);
+
+CREATE TABLE species (
+    id  SERIAL PRIMARY KEY,
+    name  VARCHAR(200) NOT NULL
+);
+
+ALTER TABLE animals
+DROP COLUMN id;
+
+ALTER TABLE animals
+ADD COLUMN id SERIAL PRIMARY KEY;
+
+ALTER TABLE animals
+DROP COLUMN species;
+
+ALTER TABLE animals
+ADD COLUMN species_id INT,
+ADD FOREIGN KEY (species_id) REFERENCES species(id);
+
+ALTER TABLE animals
+ADD COLUMN owner_id INT,
+ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
